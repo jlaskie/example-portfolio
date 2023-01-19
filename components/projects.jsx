f5b8aa4c7ad5cfce5673adf66306e03064ba8c56
jlaskie <115713394+jlaskie@users.noreply.github.com>
2023-01-19 18:51:52 +0000

    tidied up globals.css

--- components/projects.jsx
+++ components/projects.jsx
@@ -3,7 +3,7 @@ export default function Projects({ title, date, info }) {
     <div className="grid grid-flow-row justify-items-center my-8">
       <div className="max-w-xxs md:max-w-xl lg:pl-6">
         <h1>{title}</h1>
-        <h4 className="pt-2 pb-10 ">{date}</h4>
+        <h4 className="pt-2 pb-10">{date}</h4>
         <p>{info}</p>
       </div>
     </div>
